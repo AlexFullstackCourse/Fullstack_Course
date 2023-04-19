ebaeb7c9f34636ab662bacd4e42f1b15b22a68e8
AlexFullstackCourse <alexander.heidorn@padis.io>
2023-04-19 10:50:24 +0200

    Added some error handling and the methods for deleting and updating notes

--- chapters/chapter_3/index.js
+++ chapters/chapter_3/index.js
@@ -1,38 +1,34 @@
 /** Backend of the notes application */
 const express = require("express");
+const app = express();
 const cors = require("cors");
-const result = require("dotenv").config();
+require("dotenv").config();
 const Note = require("./models/note");
 
-const app = express();
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformed ID" });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({
+    error: "Unknown endpoint",
+  });
+};
+
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("build"));
-
-/** Can be removed after the delete method is defined to use the database */
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
 
 /** Should probably be deleted */
-const generateID = () => {
+/*const generateID = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
-};
+};*/
 
 /** Gets overwritten because of app.use(express.static("build")) */
 app.get("/", (request, resonse) => {
@@ -45,10 +41,21 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+  //.catch(); //(error) => {
+  //next(error);
+  /*console.log(error);
+      response.status(400).send({ error: "Malformed ID" });*/
+  /*}*/
 });
 
 app.post("/api/notes", (request, response) => {
@@ -70,20 +77,31 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-  response.status(204).end();
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({
-    error: "Unknown endpoint",
-  });
-};
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
