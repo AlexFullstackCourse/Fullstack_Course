1f9432c2e26226b6e7c98f1d97918beb2b63b579
AlexFullstackCourse <alexander.heidorn@padis.io>
2023-04-25 09:13:30 +0200

    Added some validation functionality

--- chapters/chapter_3/index.js
+++ chapters/chapter_3/index.js
@@ -10,6 +10,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformed ID" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
@@ -58,7 +60,7 @@ app.get("/api/notes/:id", (request, response, next) => {
   /*}*/
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -72,9 +74,12 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
